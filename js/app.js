080336d34aaf5164ed2ee9022e57d4da731aa6b7
mrpax0 <cristiaanduke@gmail.com>
2023-10-23 10:10:52 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -532,12 +532,11 @@ $(function() {
 
 function printArray() {
 	var msg = 'Submit new regions here:\n'
-	+ 'https://github.com/skyrossm/np-gangmap/issues\n\n'
+	+ 'Discord @mr.pax\n'
 	+ 'Right click the map to add points to the region. You may have to toggle regions off to be able to right click on the bottom layer. Fill in the values marked "<edit here>" and title the new issue using the format: "Add <title> region". Copy and paste everything below this. If your browser does not support selecting the text below press F12 to open the developer console and copy it from there. (scroll down)\n\n';
 	msg += '```json\n\t{\n\t\t"type": "Territories",'
 	+ '\n\t\t"title": "<edit this>",'
 	+ '\n\t\t"notes": "<edit this>",'
-	+ '\n\t\t"wiki_link": "https://nopixel.fandom.com/wiki/<edit this>",'
 	+ '\n\t\t"order": 0,'
 	+ '\n\t\t"strokecolor": "FF0000",'
 	+ '\n\t\t"fillcolor": "FF0000",'
